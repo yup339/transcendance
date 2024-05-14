44de84060e6c36f102a3a1be44c67c4fa22d827e
Alexandre Ouellet Fournier <aouellet@c2r2p11.42quebec.com>
2024-05-14 18:51:59 -0400

    made the pong img button

--- pong/script.js
+++ pong/script.js
@@ -2,13 +2,13 @@
 const pages = {
     'game_choice':`
     <div class="container extra-top-padding">
-    <div class="row justify-content-center mt-md-4 mt-2 mb-5 mx-md-0 mx-5 gy-4 mt-md-5 mt-3">
+    <div class="row justify-content-center mt-md-0 mt-2 mb-5 px-lg-5 gy-4 mt-md-5 mt-3">
         <h1 class="display-5 text-center fw-bold text-white ">Choose Your Game</h1>
-        <div class="col-lg-5 col-md-6 ">
-            <img src="https://via.placeholder.com/1000" class=" clickable shadow img-fluid image-button w-100 rounded-5 hover-scale" alt="Image 1" onclick="navigateTo('pong_choices')">
+        <div class="col-12 ">
+            <img src="pong_img_button.png" class=" clickable shadow img-fluid image-button img-button-width rounded-5 hover-scale d-block m-auto" alt="Image 1" onclick="navigateTo('pong_choices')">
         </div>
-        <div class="col-lg-5 col-md-6 ">
-            <img src="https://via.placeholder.com/1000" class="clickable shadow img-fluid image-button w-100 rounded-5 hover-scale" alt="Image 1" onclick="location.href='#';">
+        <div class="col-12 ">
+            <img src="pong_img_button.png" class="clickable shadow img-fluid image-button img-button-width rounded-5 hover-scale d-block m-auto" alt="Image 1" onclick="location.href='#';">
         </div>
         </div>
     </div>
