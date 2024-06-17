f5bd3ceaa5aade405cd10a638a192ba3ed9550c1
araymond6 <aurelia.raymond21@gmail.com>
2024-06-17 19:01:15 -0400

    added level gen, base platforms, player controllers, starting hitboxes

--- pong/up_script.js
+++ pong/up_script.js
@@ -13,22 +13,45 @@ let frameCount;
 //scene
 let scene;
 let renderer;
-let light;
 
 // score/players
 let playerLeft;
 let playerRight;
+
+// stats
+let jumpCount1 = 0;
+let jumpCount2 = 0;
+let distanceTravelled1 = 0;
+let distanceTravelled2 = 0;
+
+// player and player variables
+let players = [];
 let playerSpeed = 10;
 let jumpSpeed = 30;
 let isJumping = false;
-let jumpPos;
+let isFalling = false;
+let previousPos = [];
+let previousPos2 = [];
+let jumpPos = 0;
 
-// platforms
+// objects
+let light;
+let light2;
+let lightSphere
 let objects = [];
-let platforms = [];
+let objectsp2 = [];
+let platformsGeo = [
+	new THREE.BoxGeometry(5, 1, 5), // first 50 y
+	new THREE.BoxGeometry(4, 1, 5), // next 50 y, etc.. to augment difficulty
+	new THREE.BoxGeometry(3, 1, 5),
+	new THREE.BoxGeometry(2, 1, 5),
+	new THREE.BoxGeometry(1, 1, 5),
+	new THREE.BoxGeometry(0.5, 1, 5),];
+let hitboxes = [];
 let keys = {};
-let cube, material, geometry;
 let cycle = false;
+
+UpGame
 
 const views = [
 	{
@@ -36,7 +59,7 @@ const views = [
 		bottom: 0,
 		width: 0.5,
 		height: 1.0,
-		position: [0, 0, 30],
+		position: [0, 0, 28],
 		background: new THREE.Color(0xC1F7B0), // soft green
 	},
 	{
@@ -44,61 +67,10 @@ const views = [
 		bottom: 0,
 		width: 0.5,
 		height: 1.0,
-		position: [30, 0, 30],
-		background: new THREE.Color(0xF8B7EE), // purple
+		position: [30, 0, 28],
+		background: new THREE.Color(0xF8B7EE), // soft purple
 	},
 ];
-
-const players = [
-
-]
-
-
-//--------------------------------- classes
-
-class Platform 
-{
-	constructor(geometry, material, mesh)
-	{
-		this.geometry = geometry;
-		this.material = material;
-		this.mesh = mesh;
-		this.x = 0;
-		this.y = 0;
-		this.z = 0;
-	}
-}
-
-class Player
-{
-	constructor(geometry, material, mesh)
-	{
-		this.geometry = geometry;
-		this.material = material;
-		this.mesh = mesh;
-		this.x = 0;
-		this.y = 0;
-		this.z = 0;
-	}
-
-	isLanded()
-	{
-
-	}
-}
-
-class HitBox
-{
-	constructor(x, y, width, height, depth)
-	{
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-		this.depth = depth;
-	
-	}
-}
 
 function onKeyDown(event)
 {
@@ -118,7 +90,7 @@ function prepareUpGame()
 	for (let i = 0; i < views.length; ++i)
 	{
 		const view = views[i];
-		const camera = new THREE.PerspectiveCamera(40, canvas.width/canvas.height, 0.1, 500);
+		const camera = new THREE.PerspectiveCamera(50, canvas.width/canvas.height, 0.1, 500);
 		camera.position.fromArray(view.position);
 		view.camera = camera;
 	}
@@ -127,22 +99,29 @@ function prepareUpGame()
 	renderer = new THREE.WebGLRenderer({canvas: canvas});
 	renderer.setSize(canvas.width, canvas.height);
 	
+	//players
+	let geometry = new THREE.BoxGeometry(1, 1, 1);
+	let material = new THREE.MeshStandardMaterial({color: 0xF8B7EE});
+	players[0] = new THREE.Mesh(geometry, material);
+	players[0].position.set(0, -9, 0);
+	scene.add(players[0]);
+
+	let material2 = new THREE.MeshStandardMaterial({color: 0xC1F7B0});
+	players[1] = new THREE.Mesh(geometry, material2);
+	players[1].position.set(30, -9, 0);
+	scene.add(players[1]);
 	
-	geometry = new THREE.BoxGeometry(5, 1, 5);
-	material = new THREE.MeshBasicMaterial( { color: 0x7377ff } );
-	cube = new THREE.Mesh( geometry, material );
-	let cube2 = new THREE.Mesh( geometry, material ); // to remove
-	cube.position.x = 30;
-	cube.position.y = -5;
-
-	light = new THREE.PointLight( 0xffffff, 100 ); // red light
-	const helper = new THREE.PointLightHelper( light, 1 );
+	//light and its helper
+	let geo = new THREE.SphereGeometry(1, 32, 16);
+	let mat = new THREE.MeshBasicMaterial({color: 0x404040});
+	lightSphere = new THREE.Mesh(geo, mat);
+	lightSphere.position.set(0, 4, 5);
+	light = new THREE.PointLight(0x404040, 7);
+	light.position.set( 0, 4, 5 );
 	scene.add(light);
-	scene.add(helper);
-
-	scene.add(cube);
-	cube2.position.y = 5;
-	scene.add(cube2); // to remove
+	scene.add(lightSphere);
+	
+	generateLevel();
 	
 	// time
 	stop = false;
@@ -152,31 +131,101 @@ function prepareUpGame()
 	updateUpGame();
 }
 
+function countdown()
+{
+	
+}
+
+function generateLevel()
+{
+	// generate starting platforms
+	let geometry = new THREE.BoxGeometry(15, 1, 5); // starting platforms
+	let material = new THREE.MeshStandardMaterial( { color: 0x7377ff } );
+	let cube = new THREE.Mesh( geometry, material );
+	let cube2 = new THREE.Mesh( geometry, material );
+	cube.position.set(0, -10, 0);
+	cube2.position.set(30, -10, 0);
+	scene.add(cube);
+	scene.add(cube2);
+	
+
+	let platform;
+	let hitbox;
+	let y = cube.position.y + 6;
+	for (let i = 0; i < 100; i++) // TODO: make it so that x values are  not too far apart
+	{
+		if (y < 50)
+			platform = new THREE.Mesh(platformsGeo[0], material);
+		else if (y < 100)
+			platform = new THREE.Mesh(platformsGeo[1], material);
+		else if (y < 150)
+			platform = new THREE.Mesh(platformsGeo[2], material);
+		else if (y < 200)
+			platform = new THREE.Mesh(platformsGeo[3], material);
+		else if (y < 250)
+			platform = new THREE.Mesh(platformsGeo[4], material);
+		else
+			platform = new THREE.Mesh(platformsGeo[5], material);
+		platform.position.x = GetRandomInt(-5, 5);
+		platform.position.y = y;
+		hitbox = new THREE.Box3().setFromObject(platform);
+		y += 6;
+		hitboxes.push(hitbox);
+		objects.push(platform);
+		scene.add(platform);
+	}
+
+	
+}
+
 function keysEvent(elapsedTime)
 {
 	document.addEventListener('keydown', onKeyDown, false);
 	document.addEventListener('keyup', onKeyUp, false);
 
-	//key inputs
-	if (keys[37]) // left 
+	// player 1 movement
+	if (keys[65]) // a 
 	{
-		if (cube.position.x > -30)
-			cube.position.x -= playerSpeed * elapsedTime;
+		if (players[0].position.x > -7)
+			players[0].position.x -= playerSpeed * elapsedTime;
 	}
-	if (keys[39]) // right 
+	if (keys[68]) // d 
 	{
-		// if (cube.position.x < 30)
-			cube.position.x += playerSpeed * elapsedTime;
+		if (players[0].position.x < 7)
+			players[0].position.x += playerSpeed * elapsedTime;
 	}
-	if (keys[38] && isJumping == false) //up or jump
+	if (keys[87] && isJumping == false) // w
 	{
-		isJumping = true;
-		jumpPos = cube.position.y;
+		// isJumping = true;
+		// jumpPos = players[0].position.y;
+		players[0].position.y += playerSpeed * elapsedTime;
+	}
+	if (keys[83]) // s
+	{
+		players[0].position.y -= playerSpeed * elapsedTime;
+	}
+
+	// player 2 movement
+	if (keys[37]) // left
+	{
+		if (players[1].position.x > 30 - 7)
+			players[1].position.x -= playerSpeed * elapsedTime;
+	}
+	if (keys[39]) // right
+	{
+		if (players[1].position.x < 30 + 7)
+			players[1].position.x += playerSpeed * elapsedTime;
+	}
+	if (keys[38] && isJumping == false) // up
+	{
+		// isJumping = true;
+		players[1].position.y += playerSpeed * elapsedTime;
 	}
 	if (keys[40]) // down
 	{
-		cube.position.y -= playerSpeed * elapsedTime;
+		players[1].position.y -= playerSpeed * elapsedTime;
 	}
+
 }
 
 function renderUp()
@@ -194,13 +243,35 @@ function renderUp()
 		renderer.setScissor(left, bottom, width, height);
 		renderer.setScissorTest(true);
 		renderer.setClearColor(view.background);
-		// if (i == 0)
-		// 	camera.position.y = cube.position.y; // update camera position depending on player position
+		if (i == 0)
+		{
+			// lightSphere.position.y = cube.position.y + 14;
+			// light.position.y = cube.position.y + 14;
+			// camera.position.y = players[0].position.y + 10; // update camera position depending on player position
+		}
 		camera.aspect = width / height;
 		camera.updateProjectionMatrix();
 		renderer.render(scene, camera);
 	
 	}
+}
+
+function checkCollision()
+{
+	let playerBox = new THREE.Box3().setFromObject(players[0]);
+	let playerBox2 = new THREE.Box3().setFromObject(players[1]);
+	for (let i = 0; i < hitboxes.length; i++)
+	{
+		if (playerBox.intersectsBox(hitboxes[i]))
+		{
+			
+		}
+		if (playerBox2.intersectsBox(hitboxes[i]))
+		{
+			
+		}
+	}
+
 }
 
 function updateUpGame()
@@ -209,7 +280,8 @@ function updateUpGame()
 
 	// get time for current instance
 	let currentTime = performance.now();
-	console.log(Math.floor((currentTime - startTime) / 1000));
+	if (Math.floor((currentTime - startTime) / 1000))
+		console.log(Math.floor((currentTime - startTime) / 1000));
 	
 	// delta time
 	let elapsedTime = (performance.now() - lastTime) / 1000;
@@ -221,14 +293,14 @@ function updateUpGame()
 	// player logic
 	if(isJumping)
 	{
-		let nextPos = cube.position.y + jumpSpeed * elapsedTime;
+		let nextPos = players[0].position.y + jumpSpeed * elapsedTime;
 		if (nextPos > jumpPos)
-			cube.position.y = nextPos
+			players[0].position.y = nextPos
 		else
 		{
 			isJumping = false;
 			jumpSpeed = 30;
-			cube.position.y = jumpPos;
+			players[0].position.y = jumpPos;
 		}
 		if (jumpSpeed != -30)
 			jumpSpeed -= 1.5;
@@ -243,7 +315,7 @@ function GetRandomInt(min, max)
 
 function UpGame()
 {
-	game_stop = false;
+	stop = true;
 	game_mode = 'up_dual';
 
 	if(game_mode == 'up_dual')
@@ -253,21 +325,11 @@ function UpGame()
 		prepareUpGame();
 
 	}
-	else if(game_mode == 'up_ai')
-	{
-		playerLeft = 'You';
-		playerRight = 'Ai';
-		prepareUpGame();
-	}
 	else if(game_mode == 'up_online')
 	{
 		playerLeft = 'You';
 		playerRight = 'Player 2';
 		prepareOnline();
-	}
-	else if(game_mode == 'up_tournament')
-	{
-		prepare_up_tournament();
 	}
 }
 
