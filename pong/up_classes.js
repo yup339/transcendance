2d3db293cb88ac3a2bdf3d5bc34ccf55d09c9250
araymond6 <aurelia.raymond21@gmail.com>
2024-06-27 19:43:08 -0400

    made an object class containing the hitbox, mesh, x&y position, rendre flag and more

--- pong/up_classes.js
+++ pong/up_classes.js
@@ -1,10 +1,35 @@
 class Object
 {
-	constructor(mesh, x, y, color)
+	constructor(mesh, x, y, position)
 	{
 		this.x = x;
 		this.y = y;
 		this.mesh = mesh;
-		this.box3 = new THREE.Box3().setFromObject(this.mesh);
+		this.hitbox = new THREE.Box3().setFromObject(this.mesh);
+		this.position = this.mesh.position;
+		this.isRendered = false;
+	}
+
+	render(scene)
+	{
+		this.isRendered = true;
+		scene.add(this.mesh);
+	}
+
+	remove(scene)
+	{
+		this.isRendered = false;
+		scene.remove(this.mesh);
+	}
+
+	setPosition(position)
+	{
+		this.position = position;
+		
+	}
+
+	doesCollide(hitbox)
+	{
+
 	}
 }
