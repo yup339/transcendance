2d9c12627c876d477570693eac0df1ac498f8fdf
aouellet97 <123030382+aouellet97@users.noreply.github.com>
2024-07-24 21:57:01 +0000

    win rate

--- backend/transcendence/static/transcendence/js/stats.js
+++ backend/transcendence/static/transcendence/js/stats.js
@@ -11,14 +11,15 @@ function setStats(){
 
         if(element.id === 'pong_total_games'){
             element.textContent = stats.pong_online_game_played  
-                + stats.pong_offline_game_played
+                + stats.pong_offline_game_played;
         }
         else if(element.id === 'up_total_games'){
             element.textContent = stats.up_online_game_played  
-            + stats.up_offline_game_played
+            + stats.up_offline_game_played;
         }
         else if(element.id === 'win_rate'){
-            element.textContent = (stats.pong_lost > 0 ? stats.pong_won / stats.pong_lost : stats.pong_won).toFixed(2);
+            const total = stats.pong_online_game_played + stats.pong_offline_game_played;
+            element.textContent = ((total > 0 ? stats.pong_won / total : 0) * 100) + "%";
         }
         else{
             Object.keys(stats).forEach((key) => {
